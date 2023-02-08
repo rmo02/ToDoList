259e50fd6534f923c2ac8ecaba5e9def98d843f5
Ramon Maia <ramonmaia@MacBook-Pro.local>
2023-02-07 22:43:28 -0300

    fazendo a função de selecionar

---
+++ src/components/Task.tsx
@@ -0,0 +1,53 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+  onSelect: () => void;
+  selected: boolean;
+};
+
+export function Task({ name, onRemove, onSelect, selected }: Props) {
+  return (
+    <View
+      style={{
+        backgroundColor: "#1F1E25",
+        height: 50,
+        borderRadius: 12,
+        justifyContent: "space-around",
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 10,
+      }}
+    >
+      {selected ? (
+        <TouchableOpacity onPress={onSelect}>
+          <View
+            style={{
+              backgroundColor: "#5E60CE",
+              borderRadius: 25,
+              height: 24,
+              width: 24,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <FontAwesome5 name="check" size={18} color="white" />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={onSelect}>
+          <Feather name="circle" size={24} color="#1E6F9F" />
+        </TouchableOpacity>
+      )}
+
+      <Text style={{ color: "white" }}>{name}</Text>
+      <TouchableOpacity onPress={onRemove}>
+        <FontAwesome name="trash-o" size={24} color="#808080" />
+      </TouchableOpacity>
+    </View>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,42 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { Alert, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Task } from "../../components/Task";
 
 export function Home() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [taskName, setTaskName] = useState('');
+  const [selected , setSelected] = useState(false)
+  const [completed, setCompleted] = useState<string[]>([]);
+
+  function handleTaskNameAdd(){
+    if(tasks.includes(taskName)){
+      return Alert.alert("Tarefa já existente")
+    }
+    setTasks(prevState => [...prevState, taskName]);
+    setTaskName('');
+  }
+
+  function handleRemoveTask(name: string){
+    Alert.alert("Remover", `Remover a tarefa?`, [
+      {
+      text:"Sim",
+      onPress:() => setTasks(prevState => prevState.filter(task => task !== name))
+    },
+    {
+      text: "Não",
+      style:"cancel"
+    }
+  ])
+  }
+
+  
+
+  function handleSelect(item: string){
+    setSelected(true);
+    setCompleted([item])
+    console.log("clicou")
+  }
+
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
       <View style={{ alignItems: "center", marginTop: 60 }}>
@@ -28,6 +64,8 @@ export function Home() {
               marginRight: 10,
             }}
             placeholder="Adicione uma nova tarefa"
+            onChangeText={setTaskName}
+            value={taskName}
             placeholderTextColor="#6B6B6B"
           />
 
@@ -40,6 +78,7 @@ export function Home() {
               alignItems: "center",
               justifyContent: "center",
             }}
+            onPress={handleTaskNameAdd}
           >
             <Text style={{ fontSize: 24, color: "#fff" }}>+</Text>
           </TouchableOpacity>
@@ -74,7 +113,7 @@ export function Home() {
                 marginLeft: 5,
               }}
             >
-              <Text style={{ color: "white", fontWeight: "400" }}>20</Text>
+              <Text style={{ color: "white", fontWeight: "400" }}>{tasks.length}</Text>
             </View>
           </View>
           <View
@@ -96,20 +135,47 @@ export function Home() {
                 marginLeft: 5,
               }}
             >
-              <Text style={{ color: "white", fontWeight: "400" }}>20</Text>
+              <Text style={{ color: "white", fontWeight: "400" }}>{completed.length}</Text>
             </View>
           </View>
         </View>
-              <View style={{alignItems:'center', marginTop:50}}>
-              <Image style={{marginBottom:20}}
-              source={require("../../assets/Clipboard.png")} />
-        <Text style={{ color: "#808080", textAlign: "center", fontSize: 16, fontWeight:'bold' }}>
-          Adicione participantes a sua lista de presença
-        </Text>
-        <Text style={{ color: "#808080", textAlign: "center", fontSize: 16 }}>
-            Crie tarefas e organize seus itens a fazer
-        </Text>
-              </View>
+
+         <FlatList 
+          data={tasks}
+          keyExtractor={(item) => item}
+          renderItem={({item}) => (
+            <Task 
+            name={item}
+            onSelect={()=> handleSelect(item)}
+            selected={selected}
+            onRemove={()=>handleRemoveTask(item)}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={()=>(
+            <View style={{ alignItems: "center", marginTop: 100 }}>
+            <Image
+              style={{ marginBottom: 20 }}
+              source={require("../../assets/Clipboard.png")}
+            />
+            <Text
+              style={{
+                color: "#808080",
+                textAlign: "center",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Você ainda não tem tarefas cadastradas
+            </Text>
+            <Text style={{ color: "#808080", textAlign: "center", fontSize: 16 }}>
+              Crie tarefas e organize seus itens a fazer
+            </Text>
+          </View>
+          )}
+          /> 
+
+
       </View>
     </View>
   );
